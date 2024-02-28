aa076d3d11b4c04095acbdfeab27b0a999582ccf
Kabanbay1 <156914140+Kabanbay1@users.noreply.github.com>
2024-02-28 22:23:31 +0600

    Add files via upload

---
+++ regex1.py
@@ -0,0 +1,30 @@
+import re
+#1
+x1=input("Input your txt: ")
+# par=re.compile('[a][b]*')
+# print(par.findall(x1))
+#2
+# par=re.compile('[a][b]{2,3}')
+# print(par.findall(x1))
+#3
+# par=re.compile('(([a-z]+[_]+)+[a-z]{1})+')
+# print(par.findall(x1))
+#4
+# par=re.compile('[A-Z][a-z]+')
+# print(par.findall(x1))
+#5
+# par=re.compile('^a([A-z]*[a-z]*)b$')
+# print(par.findall(x1))
+#6
+# par=re.sub(r'[\s, \.]', string=x1, repl=":")
+# print(par)
+#7
+# par=re.split('_+', string)
+# par=x[0].lower() + "".join(map(lambda a: a.title(), x[1:]))
+#8
+# par=re.split("[A-Z]", string)
+# par=" ".join(x)
+#9
+# par=re.split("[A-Z]", string)
+# par=" ".join(x)
+# print(par)
